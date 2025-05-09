bc65551b7c1b2efd8c53c660aed769ccdcfcaf9c
Gaurav Padhye <gauravpadhye16@gmail.com>
2025-05-09 11:45:13 -0400

    allowing all origins

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-shopping-bot-copy.vercel.app"], allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"], allow_headers=["*"],
 )
 
